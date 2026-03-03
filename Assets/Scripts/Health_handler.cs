cda906b3a7c7b6ac6d53486991e58957c52bb909
Viktor <Viktormortvedt@gmail.com>
2026-03-03 13:29:54 +0100

    Added hit and death sounds to the enemies.

--- Assets/Scripts/Health_handler.cs
+++ Assets/Scripts/Health_handler.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Net.NetworkInformation;
 using TMPro;
 using UnityEngine;
 
@@ -8,6 +10,16 @@ public class Health_handler : MonoBehaviour
     private Transform tf; //lol
 
     private ParticleSystem par;
+
+    public AudioSource hit_1;
+
+    public AudioSource hit_2;
+
+    public AudioSource hit_3;
+
+    public AudioSource death;
+
+    private int chosen_hit_sfx;
 
     public float health = 10;
 
@@ -21,19 +33,60 @@ public class Health_handler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnEnable()
     {
         tf = GetComponent<Transform>();
         par = GetComponent<ParticleSystem>();
+        chosen_hit_sfx = Random.Range(1, 3);
+        //chosen_hit_sfx = 1; //debug
     }
 
     public void take_damage(int damage)
-    { 
+    {
         health = health - damage;
         health_counter.GetComponent<set_health>().setHealth(health, max_health);
+        if (health > 0)
+        {
+            StartCoroutine(play_hit());
+        }
+        else
+        {
+            StartCoroutine(play_death());
+        }
+    }
+    private IEnumerator play_hit()
+    {
+        float pitch = Random.Range(90, 120);
+        pitch = pitch / 100;
+        if (chosen_hit_sfx == 1)
+        {
+            hit_1.pitch = pitch;
+            hit_1.Play();
+        }
+        else if (chosen_hit_sfx == 2)
+        {
+            hit_2.pitch = pitch;
+            hit_2.Play();
+        }
+        else
+        {
+            hit_3.pitch = pitch;
+            hit_3.Play();
+        }
+        yield return new WaitForSeconds(0.2f);
+        par.Play();
+        yield return null;
+    }
+    private IEnumerator play_death()
+    {
+        float pitch = Random.Range(90, 120);
+        pitch = pitch / 100;
+        yield return null;
+        death.Play();
+        yield return new WaitForSeconds(0.2f);
         par.Play();
     }
 }
